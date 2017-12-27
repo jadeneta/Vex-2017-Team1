9de1189a6b301f56042d43e0cc20b22ab5920a8d
jadeneta <jadenmorales@gmail.com>
2017-12-27 16:36:46 -0500

    Changes from SHMS tournnament.

--- RedAuto.c
+++ RedAuto.c
@@ -1,6 +1,19 @@
+
 void RedAuto()
 {
-
-
-
+moveForwardWithSensor(1000);
+motorReq[Mogo] = -127;
+wait1Msec(1000);
+moveForwardWithSensor(300);
+motorReq[Mogo] = 127;
+wait1Msec(1000);
+turnLeftWithSensor(1800);
+moveForwardWithSensor(1000);
+turnRightWithSensor(900);
+moveForwardWithSensor(400);
+//Scream allahu akbar aasdfjoasjlfalksjd;fawljk;sadlijdsfA;lsdav./weakldsho;io;dsvHdF;KHJK;ADSVJK;ADSF AS H;DFMNLassdfkjsd;fasd;ljasljkdsklj;sdfkl;fdgf;ladsfjlkjv lkbkxmc'''''''''''''''''''asdf'sdfads'sdfasdf'asdfasjdflasdfjkhladadflkjvajkldbfdhaslkhldfdfakjdjfkhfdsahladkjfsdalkhusdasfxcmvmzxvcmn,xmzvnx,mnvm,xnczv,mnzsdmfn,sadfjsadkhwqiuerhtifqewlryoqiwuorqweysiodfh
+//motor[leftbackwheel] = 127;
+//motor[leftfrontwheel] = 127;
+//motor[rightfrontwheel] = 127;
+//motor[rightbackwheel] = 127;
 }
